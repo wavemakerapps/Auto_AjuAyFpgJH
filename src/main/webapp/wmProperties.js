1f557220b340e9da152fca9b774cc98a74d01df0
ZMlWxmArSN <automate1@wavemaker.com>
2017-10-12 20:08:21 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_AjuAyFpgJH",
+  "homePage" : "Main",
+  "name" : "Auto_AjuAyFpgJH",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
